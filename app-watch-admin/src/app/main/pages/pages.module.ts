d58885c3316881a51271bdbecc51e8cc24a2aaf2
DESKTOP-NL6PTV0\votuh <votuhai@gmail.com>
2021-12-21 23:59:24 +0700

    xóa các comp ko dùng đến

--- app-watch-admin/src/app/main/pages/pages.module.ts
+++ app-watch-admin/src/app/main/pages/pages.module.ts
@@ -8,21 +8,10 @@ import { ForgotPasswordModule } from 'app/main/pages/authentication/forgot-passw
 import { ForgotPassword2Module } from 'app/main/pages/authentication/forgot-password-2/forgot-password-2.module';
 import { ResetPasswordModule } from 'app/main/pages/authentication/reset-password/reset-password.module';
 import { ResetPassword2Module } from 'app/main/pages/authentication/reset-password-2/reset-password-2.module';
-import { LockModule } from 'app/main/pages/authentication/lock/lock.module';
-import { MailConfirmModule } from 'app/main/pages/authentication/mail-confirm/mail-confirm.module';
-import { ComingSoonModule } from 'app/main/pages/coming-soon/coming-soon.module';
-import { Error404Module } from 'app/main/pages/errors/404/error-404.module';
-import { Error500Module } from 'app/main/pages/errors/500/error-500.module';
 import { InvoiceModernModule } from 'app/main/pages/invoices/modern/modern.module';
 import { InvoiceCompactModule } from 'app/main/pages/invoices/compact/compact.module';
-import { MaintenanceModule } from 'app/main/pages/maintenance/maintenence.module';
-import { PricingModule } from 'app/main/pages/pricing/pricing.module';
-import { ProfileModule } from 'app/main/pages/profile/profile.module';
 import { SearchClassicModule } from 'app/main/pages/search/classic/search-classic.module';
 import { SearchModernModule } from 'app/main/pages/search/modern/search-modern.module';
-import { FaqModule } from 'app/main/pages/faq/faq.module';
-import { KnowledgeBaseModule } from 'app/main/pages/knowledge-base/knowledge-base.module';
-
 @NgModule({
     imports: [
         // Authentication
@@ -34,38 +23,12 @@ import { KnowledgeBaseModule } from 'app/main/pages/knowledge-base/knowledge-bas
         ForgotPassword2Module,
         ResetPasswordModule,
         ResetPassword2Module,
-        LockModule,
-        MailConfirmModule,
-
-        // Coming-soon
-        ComingSoonModule,
-
-        // Errors
-        Error404Module,
-        Error500Module,
-
         // Invoices
         InvoiceModernModule,
         InvoiceCompactModule,
-
-        // Maintenance
-        MaintenanceModule,
-
-        // Pricing
-        PricingModule,
-
-        // Profile
-        ProfileModule,
-
         // Search
         SearchClassicModule,
         SearchModernModule,
-
-        // Faq
-        FaqModule,
-
-        // Knowledge base
-        KnowledgeBaseModule
     ]
 })
 export class PagesModule
